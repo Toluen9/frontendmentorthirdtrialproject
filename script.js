552cb1f9511ac31fd79de9b5de87f3d3de099596
Toluen12 <ayodeletolulope43@gmail.com>
2025-04-11 20:54:11 +0100

    changes is css

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const navToggle = document.querySelector('.nav-hamburger');
-const navLinks = document.querySelector('.nav-links');
+// const navToggle = document.querySelector('.nav-hamburger');
+// const navLinks = document.querySelector('.nav-links');
 
-navToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
-});
+// navToggle.addEventListener('click', () => {
+//   navLinks.classList.toggle('show');
+// });
+
+if (navToggle && navLinks) {
+  navToggle.addEventListener('click', () => {
+    console.log('Hamburger clicked!');
+    navLinks.classList.toggle('show');
+  });
+}
 
